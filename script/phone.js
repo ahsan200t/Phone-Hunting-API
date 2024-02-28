5c1796cbe4ef15471ecd3f8401e2d952bedb4334
ahsan200t <ahsanpekua200@gmail.com>
2024-02-28 14:27:49 +0600

    modal section complite

--- script/phone.js
+++ script/phone.js
@@ -1,24 +1,26 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
 
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
-    displayPhone(phones);
+    displayPhone(phones, isShowAll);
 }
 
-const displayPhone = (phones) => {
+const displayPhone = (phones, isShowAll) => {
     const phoneContainer = document.getElementById("phone-container");
     phoneContainer.textContent = "";
 
     const showAllButton = document.getElementById("show-all-container");
-    if (phones.length > 12) {
+    if (phones.length > 12 && !isShowAll) {
         showAllButton.classList.remove("hidden");
     } else {
         showAllButton.classList.add("hidden");
     }
 
-    phones = phones.slice(0, 12);
+    if (!isShowAll) {
+        phones = phones.slice(0, 12);
+    }
     phones.forEach(phone => {
 
         const phoneCard = document.createElement("div");
@@ -32,36 +34,72 @@ const displayPhone = (phones) => {
       </figure>
       <div class="card-body">
         <h2 class="card-title">${phone.phone_name}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+        <p>There are many variations of passages of available, but the majority have suffered</p>
+        <p class="text-2xl font-bold text-center"><span>$</span>999</p>
+        <div class="card-actions justify-center">
+          <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
         </div>
       </div>
         `;
         phoneContainer.appendChild(phoneCard);
 
     });
-   
+
     toggleLoadingSpinner(false);
 
 }
 
 
 // handle Search Button
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
     toggleLoadingSpinner(true);
     const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
     console.log(searchText);
-    loadPhone(searchText);
+    loadPhone(searchText, isShowAll);
 }
 
 const toggleLoadingSpinner = (isLoading) => {
     const loadingSpinner = document.getElementById("loading-spinner");
-    if(isLoading){
+    if (isLoading) {
         loadingSpinner.classList.remove("hidden");
-    }else{
+    } else {
         loadingSpinner.classList.add("hidden");
     }
 }
-// loadPhone();
+
+const handleShowAll = () => {
+    handleSearch(true);
+}
+
+const handleShowDetails = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    showPhoneDetails(phone);
+}
+
+const showPhoneDetails = (phone) => {
+    const phoneName = document.getElementById("show-detail-phone-name");
+    phoneName.innerText = phone.name;
+    phoneName.classList.add("text-center");
+    const showDetailContainer = document.getElementById("show-detail-container");
+    showDetailContainer.innerHTML = `
+      <div class="flex justify-center items-center bg-[#0D6EFD0D] p-10">
+      <img src="${phone.image}"></img>
+      </div>
+      <p><span class="text-xl font-semibold">Description: </span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+      <p><span class="text-xl font-semibold">Storage: </span>${phone?.mainFeatures?.storage} </p>
+      <p><span class="text-xl font-semibold">Display-Size: </span> ${phone.mainFeatures.displaySize} </p>
+      <p><span class="text-xl font-semibold">Chipset: </span>${phone?.mainFeatures?.chipSet}</p>
+      <p><span class="text-xl font-semibold">Memory: </span>${phone?.mainFeatures?.memory}</p>
+      <p><span class="text-xl font-semibold">Slug: </span>${phone.slug}</p>
+      <p><span class="text-xl font-semibold">Release Data: </span>${phone?.releaseDate}</p>
+      <p><span class="text-xl font-semibold">Brand: </span>${phone.brand}</p>
+      <p><span class="text-xl font-semibold">GPS: </span>${phone?.others?.GPS || "NO GPS Available"}</p>
+    `;
+    show_details_modal.showModal();
+    console.log(phone)
+}
+loadPhone();
+
